8921c08acdc43a7f8933d3bc40b5df1279f74572
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2021-04-02 16:43:32 +0300

    write own distance function

--- src/electricity/Utils.java
+++ src/electricity/Utils.java
@@ -1,10 +1,12 @@
 package electricity;
 
-import java.awt.geom.Point2D;
-
 public class Utils {
 
 	public static double calcDistance(Particle p1, Particle p2) {
-		return Point2D.distance(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+		double x1 = p1.getX();
+		double y1= p1.getY();
+		x1 -= p2.getX();
+		y1 -= p2.getY();
+		return Math.sqrt(x1 * x1 + y1 * y1);
 	}
 }
